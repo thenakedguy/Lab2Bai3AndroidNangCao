4087236a6ecdec681505cf83fec3e900fada0caa
HngDng <vidang68@gmail.com>
2019-01-14 22:42:24 +0700

    đã update và sửa rất chi là nhiều thứ

--- app/src/main/java/com/hngdngcorp/hngdng/lab2bai3androidnangcao/MainActivity.java
+++ app/src/main/java/com/hngdngcorp/hngdng/lab2bai3androidnangcao/MainActivity.java
@@ -1,5 +1,7 @@
 package com.hngdngcorp.hngdng.lab2bai3androidnangcao;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
@@ -7,56 +9,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText mEdtInput;
-    private Button mButton;
-    private String input;
-    public final String ACTION_TEST = "abc";
+    private Button mButton3;
+    private TextView mTextView2;
+    private BroadcastReceiver broadcastReceiver;
+    public static final String ACTION_TEST = "abc";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mButton3 = (Button) findViewById(R.id.button3);
+        mTextView2 = (TextView) findViewById(R.id.textView2);
 
-        mEdtInput = (EditText) findViewById(R.id.edtInput);
-        mButton = (Button) findViewById(R.id.button);
-
-        mButton.setOnClickListener(new View.OnClickListener() {
+        mButton3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                check();
-                Intent intent1 = new Intent(MainActivity.this,SecondActivity.class);
-                startActivity(intent1);
-            }
-
-            private void check() {
-                input = mEdtInput.getText().toString();
-                //check so luong ki tu
-                if (input.length() == 9 ){
-                    //Check khi nào là mem, khi nào là vip, khi nào là sai
-                    if ( input.contains("MEM")){
-                        Intent intent = new Intent(ACTION_TEST);
-                        intent.putExtra("output","Chúc mừng bạn được giảm giá 15%");
-                        sendBroadcast(intent);
-                    }else if ( input.contains("VIP")){
-                        Intent intent = new Intent(ACTION_TEST);
-                        intent.putExtra("output","Chúc mừng bạn được giảm giá 50%");
-                        sendBroadcast(intent);
-                    }else {
-                        Intent intent = new Intent(ACTION_TEST);
-                        intent.putExtra("output","Bạn không được giảm giá");
-                        sendBroadcast(intent);
-                    }
-                }
-                // báo lỗi khi không nhập đủ 9 kí tự
-                else {
-                    Intent intent = new Intent(ACTION_TEST);
-                    intent.putExtra("output","Dãy nhập vào không đủ 9 kí tự");
-                    sendBroadcast(intent);
-                }
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
             }
         });
+        // Khai báo broadcast
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String text = intent.getStringExtra("output");
+                if (text!= null) mTextView2.setText(text);
+            }
+        };
+        // Đăng kí nhận intent với biến = ACTION_TEST
+        IntentFilter intentFilter = new IntentFilter(ACTION_TEST);
+        registerReceiver(broadcastReceiver,intentFilter);
     }
+
 }
--- app/src/main/java/com/hngdngcorp/hngdng/lab2bai3androidnangcao/SecondActivity.java
+++ app/src/main/java/com/hngdngcorp/hngdng/lab2bai3androidnangcao/SecondActivity.java
@@ -6,27 +6,58 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
     private TextView mTvOutput;
     public static final String ACTION_TEST = "abc";
+    private EditText mEdtInput;
+    private Button mButton;
+    private String input;
     private BroadcastReceiver broadcastReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        IntentFilter intentFilter = new IntentFilter(ACTION_TEST);
-        registerReceiver(broadcastReceiver,intentFilter);
-        mTvOutput = (TextView) findViewById(R.id.tvOutput);
+        mEdtInput = (EditText) findViewById(R.id.edtInput);
+        mButton = (Button) findViewById(R.id.button);
 
-        //Khai báo broadcastRêciver
-        broadcastReceiver= new BroadcastReceiver() {
+        mButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onReceive(Context context, Intent intent) {
-                String s = intent.getStringExtra("output");
-                if(s != null) mTvOutput.setText("abcd");
+            public void onClick(View v) {
+                check();
             }
-        } ;
+
+            private void check() {
+                input = mEdtInput.getText().toString();
+                //check so luong ki tu
+                if (input.length() == 9 ){
+                    //Check khi nào là mem, khi nào là vip, khi nào là sai
+                    if ( input.contains("MEM")){
+                        Intent intent = new Intent(ACTION_TEST);
+                        intent.putExtra("output","Chúc mừng bạn được giảm giá 15%");
+                        sendBroadcast(intent);
+                    }else if ( input.contains("VIP")){
+                        Intent intent = new Intent(ACTION_TEST);
+                        intent.putExtra("output","Chúc mừng bạn được giảm giá 50%");
+                        sendBroadcast(intent);
+                    }else {
+                        Intent intent = new Intent(ACTION_TEST);
+                        intent.putExtra("output","Bạn không được giảm giá");
+                        sendBroadcast(intent);
+                    }
+                }
+                // báo lỗi khi không nhập đủ 9 kí tự
+                else {
+                    Intent intent = new Intent(ACTION_TEST);
+                    intent.putExtra("output","Dãy nhập vào không đủ 9 kí tự");
+                    sendBroadcast(intent);
+                }
+            }
+        });
     }
 }
